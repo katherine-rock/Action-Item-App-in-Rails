41269ecd25ef07203f9223e53a80e3030c716640
katherine-rock <katherine_rock@hotmail.com>
2021-04-22 19:54:06 +1000

    Add path from landing page to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :action_items
+  root to: 'action_items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
