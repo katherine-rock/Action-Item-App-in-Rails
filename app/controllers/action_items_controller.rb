78d530da8b7d045977fb8415ae3e475fd45209a3
katherine-rock <katherine_rock@hotmail.com>
2021-04-29 19:56:28 +1000

    Add help page with link to GitHub

--- app/controllers/action_items_controller.rb
+++ app/controllers/action_items_controller.rb
@@ -56,6 +56,9 @@ class ActionItemsController < ApplicationController
     end
   end
 
+  def help
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_action_item
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :action_items
   root to: 'action_items#index'
+  get 'help', to: 'action_items#help', as: :help 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
